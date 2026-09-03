3759190ffc7f7647a1faefcb34d7cd5f2a1473ae
Riley Kivimäki <riley@wolfgirl.online>
2026-09-03 07:57:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,15 @@
 echo "Installing Dependencies..."
 sudo pacman -Sy
 sudo pacman -S git wget jre21-openjdk
-
-# Cloning
+sudo archlinux-java set java-21-openjdk
 echo "Cloning repo..."
 git clone https://github.com/xiv3r/Burpsuite-Professional.git 
 cd Burpsuite-Professional
-
-# Download Burpsuite Professional
 echo "Downloading Burp Suite..."
 version=2026
 wget -O burpsuite_pro_v$version.jar https://github.com/xiv3r/Burpsuite-Professional/releases/download/burpsuite-pro/burpsuite_pro_v$version.jar
-
-# Execute Key Generator
 echo "Starting Key loader.jar..."
 (java -jar loader.jar) &
-
-# Execute Burpsuite Professional
 echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
 chmod +x burpsuitepro
